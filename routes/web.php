2fddc82296637db5cf237112bce461b02872d129
ashikurweb <ashikurrahman7194@gmail.com>
2025-08-15 23:29:01 +0600

    refactor installer

--- routes/web.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use App\Http\Controllers\HomeController;
-use Core\Router as Route;
-
-Route::controller(HomeController::class)->group(function () {
-    Route::get('/', 'home')->name('home');
-});
-
